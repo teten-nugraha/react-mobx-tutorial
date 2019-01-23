1d4041fd184241364720f37268499a12164ad9d6
teten777 <teten.nugraha18@gmail.com>
2019-01-23 14:17:29 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form'
+import Dashboard from './components/Dashboard'
+import Reviews from './components/Reviews'
+
+import Store from './Store'
+import  {
+  decorate,
+  observable,
+  action,
+  computed
+}from 'mobx'
+
+decorate(Store, {
+  reviewList    : observable,
+  addReview     : action,
+  averageScore  : computed,
+  reviewCount   : computed,
+})
+
+const reviewStore = new Store();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Form store={reviewStore}/>
+        <Dashboard store={reviewStore}/>
+        <Reviews store={reviewStore}/>
       </div>
     );
   }
